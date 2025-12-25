3a5fc1d2b5ee82e275deb9a3b363c1ca97914764
jia <136965988+Jiaaaaaaak@users.noreply.github.com>
2025-12-26 03:31:47 +0800

    更新 報表類型

--- automation/configs/reports_types.py
+++ automation/configs/reports_types.py
@@ -1,10 +1,20 @@
+# configs/reports_types.py
 REPORT_TYPES = [
     {
         "key": "sales",
-        "path": "/analytics/sales-v2"
+        "path": "/analytics/sales-v2",
+        "download_label": "下載銷售額"
+    },
+    {
+        "key": "sales_Conversion",
+        "path": "/analytics/sales-v2",
+        "download_label": "下載轉換率"
     },
     {
         "key": "customers",
-        "path": "/analytics/customers"
+        "path": "/analytics/customers",
+        "download_label": "下載顧客群趨勢"
     }
 ]
+
+
